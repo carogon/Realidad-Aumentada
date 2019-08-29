cc36a2b785444152d12ba8afe6d177f6d4a81f35
carogon <carolina.malen.gonzalez@gmail.com>
2019-08-28 22:06:22 -0300

    App de fisica

---
+++ Fisica I/Assets/Resources/GIF/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+    public Texture2D[] frames;
+    public float framesPorSegundo = 10;
+    // Update is called once per frame
+    void Update ()
+    {
+        float index = Time.time * framesPorSegundo;
+        index = index % frames.Length;
+        GetComponent<Renderer>().material.mainTexture = frames[(int)index];
+    }
+}
